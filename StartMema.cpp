5367a1bef5377853f5b57901ef38b6888257b635
Jarek Gryczko <jarek.gryczko@gmail.com>
2012-01-27 07:01:00 +0100

    commit ostatni

--- StartMema.cpp
+++ StartMema.cpp
@@ -80,57 +80,182 @@ int main(int argc, char **arfv){
 
 
 //************************************************************************************
-    //************************************************************************************
-    // tworzy labirynt
-    //    int wybor;
-        for(int i=0;i<wysokosc;i++)
-            for(int j=0;j<szerokosc;j++) // zape³nia ca³a tablice sciana
+// tworzy labirynt
+//    int wybor;
+    for(int i=0;i<wysokosc;i++)
+        for(int j=0;j<szerokosc;j++) // zape³nia ca³a tablice sciana
+        {
+            ludek[j][i]='X';
+            tablica[j][i]='X';
+        }
+
+    for(int i=1;i<wysokosc-1;i+=2)
+        for(int j=1;j<szerokosc-1;j+=2) // Co 2 pole wstawia w tablicy wstawia puste miejsce
+        {tablica[j][i]=' ';ludek[j][i]=' ';}
+
+    for(int i=2;i<wysokosc-1;i+=2)
+        for(int j=1;j<szerokosc-1;j+=2) //Jedzie od gory i od prawej strony co 2 kratke i losuje czy ma zostac sciana czy puste miejsce
+        {
+            wybor=rand()%2;
+            if(wybor)tablica[j][i]=' ';
+            if(wybor)ludek[j][i]=' ';
+        }
+
+    for(int i=1;i<wysokosc-1;i+=2)
+        for(int j=2;j<szerokosc;j+=2)
+        {
+            wybor=rand()%2;
+            if(wybor)tablica[j][i]=' ';
+            if(wybor)ludek[j][i]=' ';
+        }
+
+    for(int i=1;i<wysokosc-1;i+=2)
+        for(int j=2;j<szerokosc;j+=2)
+        {
+            wybor=rand()%2;
+            if(wybor)tablica[j][i]=' ';
+            if(wybor)ludek[j][i]=' ';
+        }
+
+//*************************************************************************
+
+        //wyswietla labirynt
+
+
+// ***************************************************************************
+    //    szuka drogi
+
+    int idz1=1,idz2=1;
+    tablica[idz1][idz2] = 'L';
+    ludek[idz1][idz2] = 'L';
+
+    tablica[46][46] = 'M';
+    tablica[46][46] = 'M';
+//system("cls");
+//*************************************************************************
+
+        //wyswietla labirynt
+    for(int i=0;i<wysokosc;i++)
+    {
+        for(int j=0;j<szerokosc;j++)cout << tablica[i][j] ;
+        cout << endl;
+    }
+    system("Pause");
+
+// ***************************************************************************
+
+ //int lewaSciana,prawaSciana,gornaSciana,dolnaSciana;
+    bool meta,boolLewaSciana,boolPrawaSciana,boolGornaSciana,boolDolnaSciana;
+    int losuj;
+
+
+
+
+    meta=false;
+
+
+    wybor=rand()%4+1;
+while(meta== false)
+{
+    system("cls");
+    boolLewaSciana=true;
+    boolPrawaSciana=true;
+    boolGornaSciana=true;
+    boolDolnaSciana=true;
+
+    if (idz1==48 && idz2==48) {meta=true; }
+
+    int iloscscian=0;
+        if (ludek[idz1-1][idz2]==' ' || ludek[idz1-1][idz2]=='/' ) boolLewaSciana=false; else iloscscian++;
+        if (ludek[idz1][idz2-1]==' ' || ludek[idz1][idz2-1]=='/' ) boolGornaSciana=false; else iloscscian++;
+        if (ludek[idz1+1][idz2]==' ' || ludek[idz1+1][idz2]=='/') boolPrawaSciana=false; else iloscscian++;
+        if (ludek[idz1][idz2+1]==' ' || ludek[idz1][idz2+1]=='/') boolDolnaSciana=false; else iloscscian++;
+
+    if ( iloscscian == 4 ) {cout << "nie ma ruchu zly labirynt";  break; }
+        else if (iloscscian == 3)
             {
-                ludek[j][i]='X';
-                tablica[j][i]='X';
+                if (ludek[idz1-1][idz2]==' ' || ludek[idz1-1][idz2]=='/') {tablica[idz1][idz2]='/'; tablica[idz1-1][idz2]='L'; ludek[idz1][idz2]='*'; idz1-=1;}
+                if (ludek[idz1][idz2-1]==' ' || ludek[idz1][idz2-1]=='/') {tablica[idz1][idz2]='/'; tablica[idz1][idz2-1]='L'; ludek[idz1][idz2]='*'; idz2-=1;}
+                if (ludek[idz1+1][idz2]==' ' || ludek[idz1+1][idz2]=='/') {tablica[idz1][idz2]='/'; tablica[idz1+1][idz2]='L'; ludek[idz1][idz2]='*'; idz1+=1;}
+                if (ludek[idz1][idz2+1]==' ' || ludek[idz1][idz2+1]=='/') {tablica[idz1][idz2]='/'; tablica[idz1][idz2+1]='L'; ludek[idz1][idz2]='*'; idz2+=1;}
+                wybor=rand()%4+1;
+            }
+            else {
+                losuj=0;
+                while(losuj==0)
+                {
+                    //wybor=rand()%4+1;
+                        switch(wybor)
+                    {
+                        case 1://lewo
+                        if(boolLewaSciana==false){
+                        tablica[idz1][idz2]='/';
+                        tablica[idz1-1][idz2]='L';
+                        idz1-=1;
+                        losuj++;
+                        }else wybor=rand()%4+1;
+                        break;
+                        case 2://gora
+                        if(boolGornaSciana==false){
+                        tablica[idz1][idz2]='/';
+                        tablica[idz1][idz2-1]='L';
+                        idz2-=1;
+                        losuj++;
+                        }else wybor=rand()%4+1;
+                        break;
+                        case 3:
+                        if(boolPrawaSciana==false){
+                        tablica[idz1][idz2]='/';
+                        tablica[idz1+1][idz2]='L';
+                        idz1+=1;
+                        losuj++;
+                        }else wybor=rand()%4+1;
+                        break;
+                        case 4:
+                        if(boolDolnaSciana==false){
+                        tablica[idz1][idz2]='/';
+                        tablica[idz1][idz2+1]='L';
+                        idz2+=1;
+                        losuj++;
+                        }else wybor=rand()%4+1;
+                        break;
+
+                    }
+
+
+                }
             }
 
-        for(int i=1;i<wysokosc-1;i+=2)
-            for(int j=1;j<szerokosc-1;j+=2) // Co 2 pole wstawia w tablicy wstawia puste miejsce
-            {tablica[j][i]=' ';ludek[j][i]=' ';}
 
-        for(int i=2;i<wysokosc-1;i+=2)
-            for(int j=1;j<szerokosc-1;j+=2) //Jedzie od gory i od prawej strony co 2 kratke i losuje czy ma zostac sciana czy puste miejsce
-            {
-                wybor=rand()%2;
-                if(wybor)tablica[j][i]=' ';
-                if(wybor)ludek[j][i]=' ';
-            }
+//*************************************************************************
 
-        for(int i=1;i<wysokosc-1;i+=2)
-            for(int j=2;j<szerokosc;j+=2)
-            {
-                wybor=rand()%2;
-                if(wybor)tablica[j][i]=' ';
-                if(wybor)ludek[j][i]=' ';
-            }
+        //wyswietla labirynt
+    for(int i=0;i<wysokosc;i++)
+    {
+        for(int j=0;j<szerokosc;j++)cout << tablica[i][j] ;
+        cout << endl;
+    }
 
-        for(int i=1;i<wysokosc-1;i+=2)
-            for(int j=2;j<szerokosc;j+=2)
-            {
-                wybor=rand()%2;
-                if(wybor)tablica[j][i]=' ';
-                if(wybor)ludek[j][i]=' ';
-            }
+// ***************************************************************************
 
-    //*************************************************************************
-
-            //wyswietla labirynt
+    Sleep(1000);
+//system("Pause");
+}
 
 
-    // ***************************************************************************
-        //    szuka drogi
 
-        int idz1=1,idz2=1;
-        tablica[idz1][idz2] = 'L';
-        ludek[idz1][idz2] = 'L';
 
-        tablica[46][46] = 'M';
-        tablica[46][46] = 'M';
-    //system("cls");
-    //*************************************************************************
+
+system("Pause");
+
+
+
+
+
+
+
+
+
+
+
+}
