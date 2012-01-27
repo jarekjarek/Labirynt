bcc370e4ee52bba24021bb73b902b6393214405e
Jarek Gryczko <jarek.gryczko@gmail.com>
2012-01-27 06:37:48 +0100

    first commit

---
+++ StartMema.cpp
@@ -0,0 +1,65 @@
+//#include <sdl\SDL.h>
+
+// jest to wstep do zrobienia algorytmu ale do bani :P
+#include <conio.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+using namespace std;
+
+const int szerokosc = 49;  
+const int wysokosc = 49;
+
+
+/*
+void StartMema(char tablica[szerokosc][wysokosc]){ // wybranie startu i mety mozna zmienic
+
+    int wiersz,kolumna;
+
+    cout << "gdzie ma bys start ? ";
+            cin >> wiersz;
+            while (wiersz < 0 || wiersz >= szerokosc)
+            {
+                cout << "Wybierz wiersz od 0 do " << (szerokosc - 1) << ".\n";
+                cout << "wiersz? ";
+                cin >> wiersz;
+            }
+
+            cout << "kolumna? ";
+            cin >> kolumna;
+            while (kolumna < 0 || kolumna >= wysokosc)
+            {
+                cout << "Wybierz kolumne od 0 do " << (wysokosc - 1) << ".\n";
+                cout << "kolumna? ";
+                cin >> kolumna;
+            }
+
+            tablica[wiersz][kolumna]='S'; //wejscie
+
+            cout << "gdzie ma bys meta ? ";
+            cin >> wiersz;
+            while (wiersz < 0 || wiersz >= szerokosc)
+            {
+                cout << "Wybierz wiersz od 0 do " << (szerokosc - 1) << ".\n";
+                cout << "wiersz? ";
+                cin >> wiersz;
+            }
+
+            cout << "kolumna? ";
+            cin >> kolumna;
+            while (kolumna < 0 || kolumna >= wysokosc)
+            {
+                cout << "Wybierz kolumne od 0 do " << (wysokosc - 1) << ".\n";
+                cout << "kolumna? ";
+                cin >> kolumna;
+            }
+
+     
+    tablica[wiersz][kolumna]='M'; // wyjscie
+
+}
+*/
